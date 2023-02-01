5af45fb4f4394db01e96b531c2687d150d5644ab
Christian Schuhmann <97274+madebyherzblut@users.noreply.github.com>
2023-02-01 17:36:07 +0100

    fix: Export types from package

--- scripts/build.cjs
+++ scripts/build.cjs
@@ -1,12 +1,14 @@
 #!/usr/bin/env node
+const path = require("path");
 const { build, ts, tsconfig, dirname, glob, log } = require("estrella");
 
 const buildOptions = {
+  cwd: path.join(__dirname, ".."),
   bundle: true,
   minify: process.env.NODE_ENV === "production",
   platform: "node",
   external: ["graphql", "relay-config"],
-  target: "node14",
+  target: "node16",
 };
 
 build({
@@ -14,6 +16,10 @@ build({
   entry: "src/index.ts",
   outfile: "lib/index.mjs",
   format: "esm",
+  onEnd(config) {
+    const dtsFilesOutdir = dirname(config.outfile);
+    generateTypeDefs(tsconfig(config), config.entry, dtsFilesOutdir);
+  },
 });
 
 build({
@@ -23,28 +29,26 @@ build({
   format: "cjs",
 });
 
-// function generateTypeDefs(tsconfig, entryfiles, outdir) {
-//   console.log(tsconfig);
+function generateTypeDefs(tsconfig, entryfiles, outdir) {
+  const filenames = Array.from(
+    new Set(
+      (Array.isArray(entryfiles) ? entryfiles : [entryfiles]).concat(
+        tsconfig.include || []
+      )
+    )
+  ).filter(Boolean);
 
-//   const filenames = Array.from(
-//     new Set(
-//       (Array.isArray(entryfiles) ? entryfiles : [entryfiles]).concat(
-//         tsconfig.include || []
-//       )
-//     )
-//   ).filter(Boolean);
+  log.info("Generating type declaration files for", filenames.join(", "));
 
-//   log.info("Generating type declaration files for", filenames.join(", "));
+  const compilerOptions = {
+    ...tsconfig.compilerOptions,
+    moduleResolution: undefined,
+    declaration: true,
+    outDir: outdir,
+  };
 
-//   const compilerOptions = {
-//     ...tsconfig.compilerOptions,
-//     moduleResolution: undefined,
-//     declaration: true,
-//     outDir: outdir,
-//   };
+  const program = ts.ts.createProgram(filenames, compilerOptions);
+  program.emit(undefined, undefined, undefined, true);
 
-//   const program = ts.ts.createProgram(filenames, compilerOptions);
-//   program.emit(undefined, undefined, undefined, true);
-
-//   log.info("Wrote", glob(outdir + "/*.d.ts").join(", "));
-// }
+  log.info("Wrote", glob(outdir + "/*.d.ts").join(", "));
+}
